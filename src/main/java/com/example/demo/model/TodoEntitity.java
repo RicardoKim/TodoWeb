91a49e20df685c46af54afe3d70827f6d5258bad
RicardoKim <may3210@naver.com>
2021-12-17 11:38:22 +0900

    Create Package(com.example.demo.model), Create Class(TodoEntity.java)

--- src/main/java/com/example/demo/model/TodoEntitity.java
+++ src/main/java/com/example/demo/model/TodoEntitity.java
@@ -10,7 +10,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor // 클래스 내부의 모든 변수가 매개변수로 받는 생성자임을 의미한다.
 @Data // Getter와 Setter를 클래스 내부의 메소드로 정의하지 않아도 정의가 되게 해주는 어노테이션이다.
 public class TodoEntitity {
-	private String id;
-	private String userId;
-	private String title;
+	private String id; // 이 오브젝트의 아이디 , 즉 데이터베이스의 고유 값이라고 생각이 든다.
+	private String userId; // 이 오브젝트를 생성한 사용자의 아이디
+	private String title; // Todo 타이틀
+	private boolean done; // true - todo를 완료한 경우
 }
