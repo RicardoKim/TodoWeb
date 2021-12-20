986bee0b3587e41db0781d1a50c751fd78d43b73
RicardoKim <may3210@naver.com>
2021-12-20 11:01:34 +0900

    Add Client and finish test

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import TodoComponent from './components/Todo';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoComponent/>
+    </div>
+  );
+}
+
+export default App;
